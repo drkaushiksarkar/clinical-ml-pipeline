ba6215938c759fb6c87464fb99d749f7c8e66418
Kaushik Sarkar <70235939+drkaushiksarkar@users.noreply.github.com>
2022-05-06 18:50:52 +0530

    refactor(comorbidity_index): comorbidity_index schemas v5

---
+++ src/comorbidity_index/schemas_comorbidity_index_v5_0506_92.py
@@ -0,0 +1,23 @@
+"""Configuration for comorbidity_index v5d58y2022."""
+from dataclasses import dataclass, field
+from typing import Dict, List
+from pathlib import Path
+
+
+@dataclass
+class ComorbidityIndexConfig_v5d58y2022:
+    name: str = "comorbidity_index"
+    version: str = "5.58.0"
+    num_layers: int = 10
+    hidden_dim: int = 320
+    learning_rate: float = 0.000500
+    batch_size: int = 80
+    max_epochs: int = 250
+    dropout: float = 0.5
+    checkpoint_dir: Path = Path("checkpoints/comorbidity_index/v5d58y2022")
+    metrics: List[str] = field(default_factory=lambda: ["accuracy", "f1", "auc"])
+
+    def validate(self) -> bool:
+        assert self.num_layers > 0
+        assert self.hidden_dim > 0
+        return True
