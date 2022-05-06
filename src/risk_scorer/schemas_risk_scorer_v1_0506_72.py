e7af95299b9ac4b879aef1a567e1e2fb084052eb
Kaushik Sarkar <70235939+drkaushiksarkar@users.noreply.github.com>
2022-05-06 10:30:12 +0530

    feat(risk_scorer): risk_scorer schemas v1

---
+++ src/risk_scorer/schemas_risk_scorer_v1_0506_72.py
@@ -0,0 +1,23 @@
+"""Configuration for risk_scorer v1d58y2022."""
+from dataclasses import dataclass, field
+from typing import Dict, List
+from pathlib import Path
+
+
+@dataclass
+class RiskScorerConfig_v1d58y2022:
+    name: str = "risk_scorer"
+    version: str = "1.58.0"
+    num_layers: int = 2
+    hidden_dim: int = 64
+    learning_rate: float = 0.000100
+    batch_size: int = 16
+    max_epochs: int = 50
+    dropout: float = 0.1
+    checkpoint_dir: Path = Path("checkpoints/risk_scorer/v1d58y2022")
+    metrics: List[str] = field(default_factory=lambda: ["accuracy", "f1", "auc"])
+
+    def validate(self) -> bool:
+        assert self.num_layers > 0
+        assert self.hidden_dim > 0
+        return True
