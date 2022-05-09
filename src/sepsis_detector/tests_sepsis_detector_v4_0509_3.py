e3ef696ee62625fcbb04d2b029e9de6c0710b4dd
Kaushik Sarkar <70235939+drkaushiksarkar@users.noreply.github.com>
2022-05-09 20:03:59 +0530

    test(sepsis_detector): sepsis_detector tests v4

---
+++ src/sepsis_detector/tests_sepsis_detector_v4_0509_3.py
@@ -0,0 +1,24 @@
+"""Tests for sepsis_detector v4d59y2022."""
+import pytest
+import numpy as np
+
+
+class TestSepsisDetector_v4d59y2022:
+    def test_init(self):
+        config = {"domain": "sepsis_detector", "v": 4}
+        assert config["v"] == 4
+
+    def test_forward(self):
+        x = np.random.randn(16, 32)
+        y = np.maximum(0, x)
+        assert y.shape == x.shape
+
+    def test_batch(self):
+        batch = [np.random.randn(10) for _ in range(12)]
+        assert len(batch) == 12
+
+    def test_metric(self):
+        pred = np.random.randn(32)
+        target = np.random.randn(32)
+        mse = float(np.mean((pred - target) ** 2))
+        assert mse >= 0
