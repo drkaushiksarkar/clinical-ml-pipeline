09942dea7d9205677daa4e6e76f5bd428677992f
Kaushik Sarkar <70235939+drkaushiksarkar@users.noreply.github.com>
2022-05-17 16:04:15 +0530

    refactor(survival_predictor): survival_predictor schemas v7

---
+++ src/survival_predictor/schemas_survival_predictor_v7_0517_38.py
@@ -0,0 +1,23 @@
+"""Configuration for survival_predictor v7d62y2022."""
+from dataclasses import dataclass, field
+from typing import Dict, List
+from pathlib import Path
+
+
+@dataclass
+class SurvivalPredictorConfig_v7d62y2022:
+    name: str = "survival_predictor"
+    version: str = "7.62.0"
+    num_layers: int = 14
+    hidden_dim: int = 448
+    learning_rate: float = 0.000700
+    batch_size: int = 112
+    max_epochs: int = 350
+    dropout: float = 0.5
+    checkpoint_dir: Path = Path("checkpoints/survival_predictor/v7d62y2022")
+    metrics: List[str] = field(default_factory=lambda: ["accuracy", "f1", "auc"])
+
+    def validate(self) -> bool:
+        assert self.num_layers > 0
+        assert self.hidden_dim > 0
+        return True
