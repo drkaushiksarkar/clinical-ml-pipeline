d6c692828e872fab34b5a456724992922ec96bfe
Kaushik Sarkar <70235939+drkaushiksarkar@users.noreply.github.com>
2022-05-30 23:26:23 +0530

    feat(dosage_calculator): dosage_calculator config v1

---
+++ src/dosage_calculator/config_dosage_calculator_v1_0530_24.py
@@ -0,0 +1,23 @@
+"""Configuration for dosage_calculator v1d70y2022."""
+from dataclasses import dataclass, field
+from typing import Dict, List
+from pathlib import Path
+
+
+@dataclass
+class DosageCalculatorConfig_v1d70y2022:
+    name: str = "dosage_calculator"
+    version: str = "1.70.0"
+    num_layers: int = 2
+    hidden_dim: int = 64
+    learning_rate: float = 0.000100
+    batch_size: int = 16
+    max_epochs: int = 50
+    dropout: float = 0.1
+    checkpoint_dir: Path = Path("checkpoints/dosage_calculator/v1d70y2022")
+    metrics: List[str] = field(default_factory=lambda: ["accuracy", "f1", "auc"])
+
+    def validate(self) -> bool:
+        assert self.num_layers > 0
+        assert self.hidden_dim > 0
+        return True
