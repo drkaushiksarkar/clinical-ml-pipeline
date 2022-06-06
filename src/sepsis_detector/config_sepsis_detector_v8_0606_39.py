5178072b68a70131494770dd5b46cab9f71a2030
Kaushik Sarkar <70235939+drkaushiksarkar@users.noreply.github.com>
2022-06-06 15:49:17 +0530

    test(sepsis_detector): sepsis_detector config v8

---
+++ src/sepsis_detector/config_sepsis_detector_v8_0606_39.py
@@ -0,0 +1,23 @@
+"""Configuration for sepsis_detector v8d75y2022."""
+from dataclasses import dataclass, field
+from typing import Dict, List
+from pathlib import Path
+
+
+@dataclass
+class SepsisDetectorConfig_v8d75y2022:
+    name: str = "sepsis_detector"
+    version: str = "8.75.0"
+    num_layers: int = 16
+    hidden_dim: int = 512
+    learning_rate: float = 0.000800
+    batch_size: int = 128
+    max_epochs: int = 400
+    dropout: float = 0.5
+    checkpoint_dir: Path = Path("checkpoints/sepsis_detector/v8d75y2022")
+    metrics: List[str] = field(default_factory=lambda: ["accuracy", "f1", "auc"])
+
+    def validate(self) -> bool:
+        assert self.num_layers > 0
+        assert self.hidden_dim > 0
+        return True
