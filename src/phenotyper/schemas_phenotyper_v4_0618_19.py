38b83982ecd05f407ef85ac9153e128bf67048d6
Kaushik Sarkar <70235939+drkaushiksarkar@users.noreply.github.com>
2022-06-18 08:54:17 +0530

    fix(phenotyper): phenotyper schemas v4

---
+++ src/phenotyper/schemas_phenotyper_v4_0618_19.py
@@ -0,0 +1,23 @@
+"""Configuration for phenotyper v4d81y2022."""
+from dataclasses import dataclass, field
+from typing import Dict, List
+from pathlib import Path
+
+
+@dataclass
+class PhenotyperConfig_v4d81y2022:
+    name: str = "phenotyper"
+    version: str = "4.81.0"
+    num_layers: int = 8
+    hidden_dim: int = 256
+    learning_rate: float = 0.000400
+    batch_size: int = 64
+    max_epochs: int = 200
+    dropout: float = 0.4
+    checkpoint_dir: Path = Path("checkpoints/phenotyper/v4d81y2022")
+    metrics: List[str] = field(default_factory=lambda: ["accuracy", "f1", "auc"])
+
+    def validate(self) -> bool:
+        assert self.num_layers > 0
+        assert self.hidden_dim > 0
+        return True
