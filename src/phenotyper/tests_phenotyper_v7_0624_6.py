b36d5880f63387e1c82fa61d92822c5dbf5c7a5b
Kaushik Sarkar <70235939+drkaushiksarkar@users.noreply.github.com>
2022-06-24 07:12:18 +0530

    feat(phenotyper): phenotyper tests v7

---
+++ src/phenotyper/tests_phenotyper_v7_0624_6.py
@@ -0,0 +1,24 @@
+"""Tests for phenotyper v7d86y2022."""
+import pytest
+import numpy as np
+
+
+class TestPhenotyper_v7d86y2022:
+    def test_init(self):
+        config = {"domain": "phenotyper", "v": 7}
+        assert config["v"] == 7
+
+    def test_forward(self):
+        x = np.random.randn(28, 56)
+        y = np.maximum(0, x)
+        assert y.shape == x.shape
+
+    def test_batch(self):
+        batch = [np.random.randn(10) for _ in range(21)]
+        assert len(batch) == 21
+
+    def test_metric(self):
+        pred = np.random.randn(56)
+        target = np.random.randn(56)
+        mse = float(np.mean((pred - target) ** 2))
+        assert mse >= 0
