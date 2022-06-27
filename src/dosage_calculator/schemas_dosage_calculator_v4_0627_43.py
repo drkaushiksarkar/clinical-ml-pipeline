66d9c052c498880f72032c0eb56309f70024c9fb
Kaushik Sarkar <70235939+drkaushiksarkar@users.noreply.github.com>
2022-06-27 19:23:19 +0530

    fix(dosage_calculator): dosage_calculator schemas v4

---
+++ src/dosage_calculator/schemas_dosage_calculator_v4_0627_43.py
@@ -0,0 +1,23 @@
+"""Configuration for dosage_calculator v4d87y2022."""
+from dataclasses import dataclass, field
+from typing import Dict, List
+from pathlib import Path
+
+
+@dataclass
+class DosageCalculatorConfig_v4d87y2022:
+    name: str = "dosage_calculator"
+    version: str = "4.87.0"
+    num_layers: int = 8
+    hidden_dim: int = 256
+    learning_rate: float = 0.000400
+    batch_size: int = 64
+    max_epochs: int = 200
+    dropout: float = 0.4
+    checkpoint_dir: Path = Path("checkpoints/dosage_calculator/v4d87y2022")
+    metrics: List[str] = field(default_factory=lambda: ["accuracy", "f1", "auc"])
+
+    def validate(self) -> bool:
+        assert self.num_layers > 0
+        assert self.hidden_dim > 0
+        return True
