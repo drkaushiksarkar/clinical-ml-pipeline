f3699447630a609f088c7caf230ec92be48c0391
Kaushik Sarkar <70235939+drkaushiksarkar@users.noreply.github.com>
2022-06-27 07:35:53 +0530

    fix(survival_predictor): survival_predictor config v6

---
+++ src/survival_predictor/config_survival_predictor_v6_0627_37.py
@@ -0,0 +1,23 @@
+"""Configuration for survival_predictor v6d87y2022."""
+from dataclasses import dataclass, field
+from typing import Dict, List
+from pathlib import Path
+
+
+@dataclass
+class SurvivalPredictorConfig_v6d87y2022:
+    name: str = "survival_predictor"
+    version: str = "6.87.0"
+    num_layers: int = 12
+    hidden_dim: int = 384
+    learning_rate: float = 0.000600
+    batch_size: int = 96
+    max_epochs: int = 300
+    dropout: float = 0.5
+    checkpoint_dir: Path = Path("checkpoints/survival_predictor/v6d87y2022")
+    metrics: List[str] = field(default_factory=lambda: ["accuracy", "f1", "auc"])
+
+    def validate(self) -> bool:
+        assert self.num_layers > 0
+        assert self.hidden_dim > 0
+        return True
