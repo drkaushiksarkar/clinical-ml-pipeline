8e0ed37077cca0abc045e907900a6749343f4a71
Kaushik Sarkar <70235939+drkaushiksarkar@users.noreply.github.com>
2022-06-29 16:43:28 +0530

    feat(medication_reconciler): medication_reconciler config v7

---
+++ src/medication_reconciler/config_medication_reconciler_v7_0629_6.py
@@ -0,0 +1,23 @@
+"""Configuration for medication_reconciler v7d88y2022."""
+from dataclasses import dataclass, field
+from typing import Dict, List
+from pathlib import Path
+
+
+@dataclass
+class MedicationReconcilerConfig_v7d88y2022:
+    name: str = "medication_reconciler"
+    version: str = "7.88.0"
+    num_layers: int = 14
+    hidden_dim: int = 448
+    learning_rate: float = 0.000700
+    batch_size: int = 112
+    max_epochs: int = 350
+    dropout: float = 0.5
+    checkpoint_dir: Path = Path("checkpoints/medication_reconciler/v7d88y2022")
+    metrics: List[str] = field(default_factory=lambda: ["accuracy", "f1", "auc"])
+
+    def validate(self) -> bool:
+        assert self.num_layers > 0
+        assert self.hidden_dim > 0
+        return True
