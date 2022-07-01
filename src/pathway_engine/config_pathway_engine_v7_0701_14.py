ea85ee8b56b375d06029620e7b9a63038234d2ea
Kaushik Sarkar <70235939+drkaushiksarkar@users.noreply.github.com>
2022-07-01 15:39:39 +0530

    refactor(pathway_engine): pathway_engine config v7

---
+++ src/pathway_engine/config_pathway_engine_v7_0701_14.py
@@ -0,0 +1,23 @@
+"""Configuration for pathway_engine v7d90y2022."""
+from dataclasses import dataclass, field
+from typing import Dict, List
+from pathlib import Path
+
+
+@dataclass
+class PathwayEngineConfig_v7d90y2022:
+    name: str = "pathway_engine"
+    version: str = "7.90.0"
+    num_layers: int = 14
+    hidden_dim: int = 448
+    learning_rate: float = 0.000700
+    batch_size: int = 112
+    max_epochs: int = 350
+    dropout: float = 0.5
+    checkpoint_dir: Path = Path("checkpoints/pathway_engine/v7d90y2022")
+    metrics: List[str] = field(default_factory=lambda: ["accuracy", "f1", "auc"])
+
+    def validate(self) -> bool:
+        assert self.num_layers > 0
+        assert self.hidden_dim > 0
+        return True
