0ce52e8b50b224e96246ec309805116324b2f894
Kaushik Sarkar <70235939+drkaushiksarkar@users.noreply.github.com>
2022-07-07 16:34:35 +0530

    perf(pathway_engine): pathway_engine schemas v5

---
+++ src/pathway_engine/schemas_pathway_engine_v5_0707_76.py
@@ -0,0 +1,23 @@
+"""Configuration for pathway_engine v5d94y2022."""
+from dataclasses import dataclass, field
+from typing import Dict, List
+from pathlib import Path
+
+
+@dataclass
+class PathwayEngineConfig_v5d94y2022:
+    name: str = "pathway_engine"
+    version: str = "5.94.0"
+    num_layers: int = 10
+    hidden_dim: int = 320
+    learning_rate: float = 0.000500
+    batch_size: int = 80
+    max_epochs: int = 250
+    dropout: float = 0.5
+    checkpoint_dir: Path = Path("checkpoints/pathway_engine/v5d94y2022")
+    metrics: List[str] = field(default_factory=lambda: ["accuracy", "f1", "auc"])
+
+    def validate(self) -> bool:
+        assert self.num_layers > 0
+        assert self.hidden_dim > 0
+        return True
