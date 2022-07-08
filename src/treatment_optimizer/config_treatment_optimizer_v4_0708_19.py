09c70b19cb201af11146f1f0e41378ffde9d4509
Kaushik Sarkar <70235939+drkaushiksarkar@users.noreply.github.com>
2022-07-08 18:00:29 +0530

    fix(treatment_optimizer): treatment_optimizer config v4

---
+++ src/treatment_optimizer/config_treatment_optimizer_v4_0708_19.py
@@ -0,0 +1,23 @@
+"""Configuration for treatment_optimizer v4d95y2022."""
+from dataclasses import dataclass, field
+from typing import Dict, List
+from pathlib import Path
+
+
+@dataclass
+class TreatmentOptimizerConfig_v4d95y2022:
+    name: str = "treatment_optimizer"
+    version: str = "4.95.0"
+    num_layers: int = 8
+    hidden_dim: int = 256
+    learning_rate: float = 0.000400
+    batch_size: int = 64
+    max_epochs: int = 200
+    dropout: float = 0.4
+    checkpoint_dir: Path = Path("checkpoints/treatment_optimizer/v4d95y2022")
+    metrics: List[str] = field(default_factory=lambda: ["accuracy", "f1", "auc"])
+
+    def validate(self) -> bool:
+        assert self.num_layers > 0
+        assert self.hidden_dim > 0
+        return True
