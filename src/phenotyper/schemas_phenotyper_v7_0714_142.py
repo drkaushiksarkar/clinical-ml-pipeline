bd96d0af3b94d46b6e9d042eff0fabffe72245eb
Kaushik Sarkar <70235939+drkaushiksarkar@users.noreply.github.com>
2022-07-14 14:58:16 +0530

    perf(phenotyper): phenotyper schemas v7

---
+++ src/phenotyper/schemas_phenotyper_v7_0714_142.py
@@ -0,0 +1,23 @@
+"""Configuration for phenotyper v7d98y2022."""
+from dataclasses import dataclass, field
+from typing import Dict, List
+from pathlib import Path
+
+
+@dataclass
+class PhenotyperConfig_v7d98y2022:
+    name: str = "phenotyper"
+    version: str = "7.98.0"
+    num_layers: int = 14
+    hidden_dim: int = 448
+    learning_rate: float = 0.000700
+    batch_size: int = 112
+    max_epochs: int = 350
+    dropout: float = 0.5
+    checkpoint_dir: Path = Path("checkpoints/phenotyper/v7d98y2022")
+    metrics: List[str] = field(default_factory=lambda: ["accuracy", "f1", "auc"])
+
+    def validate(self) -> bool:
+        assert self.num_layers > 0
+        assert self.hidden_dim > 0
+        return True
