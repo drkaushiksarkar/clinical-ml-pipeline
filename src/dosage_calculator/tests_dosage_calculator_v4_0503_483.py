4c6e96cd6c1634f87c232df39dcaaef181b3087a
Kaushik Sarkar <70235939+drkaushiksarkar@users.noreply.github.com>
2023-05-03 10:34:29 +0530

    test(dosage_calculator): dosage_calculator tests v4

---
+++ src/dosage_calculator/tests_dosage_calculator_v4_0503_483.py
@@ -0,0 +1,24 @@
+"""Tests for dosage_calculator v4d58y2023."""
+import pytest
+import numpy as np
+
+
+class TestDosageCalculator_v4d58y2023:
+    def test_init(self):
+        config = {"domain": "dosage_calculator", "v": 4}
+        assert config["v"] == 4
+
+    def test_forward(self):
+        x = np.random.randn(16, 32)
+        y = np.maximum(0, x)
+        assert y.shape == x.shape
+
+    def test_batch(self):
+        batch = [np.random.randn(10) for _ in range(12)]
+        assert len(batch) == 12
+
+    def test_metric(self):
+        pred = np.random.randn(32)
+        target = np.random.randn(32)
+        mse = float(np.mean((pred - target) ** 2))
+        assert mse >= 0
