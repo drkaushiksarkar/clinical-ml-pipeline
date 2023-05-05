925668a775692ff04b29231fb289319b12d7b050
Kaushik Sarkar <70235939+drkaushiksarkar@users.noreply.github.com>
2023-05-05 12:35:11 +0530

    test(dosage_calculator): dosage_calculator schemas v8

---
+++ src/dosage_calculator/schemas_dosage_calculator_v8_0505_39.py
@@ -0,0 +1,23 @@
+"""Configuration for dosage_calculator v8d60y2023."""
+from dataclasses import dataclass, field
+from typing import Dict, List
+from pathlib import Path
+
+
+@dataclass
+class DosageCalculatorConfig_v8d60y2023:
+    name: str = "dosage_calculator"
+    version: str = "8.60.0"
+    num_layers: int = 16
+    hidden_dim: int = 512
+    learning_rate: float = 0.000800
+    batch_size: int = 128
+    max_epochs: int = 400
+    dropout: float = 0.5
+    checkpoint_dir: Path = Path("checkpoints/dosage_calculator/v8d60y2023")
+    metrics: List[str] = field(default_factory=lambda: ["accuracy", "f1", "auc"])
+
+    def validate(self) -> bool:
+        assert self.num_layers > 0
+        assert self.hidden_dim > 0
+        return True
