5d62a7daa902101720fad1f755b41e631175d9e4
Kaushik Sarkar <70235939+drkaushiksarkar@users.noreply.github.com>
2023-05-12 22:42:25 +0530

    fix(phenotyper): phenotyper config v8

---
+++ src/phenotyper/config_phenotyper_v8_0512_79.py
@@ -0,0 +1,23 @@
+"""Configuration for phenotyper v8d64y2023."""
+from dataclasses import dataclass, field
+from typing import Dict, List
+from pathlib import Path
+
+
+@dataclass
+class PhenotyperConfig_v8d64y2023:
+    name: str = "phenotyper"
+    version: str = "8.64.0"
+    num_layers: int = 16
+    hidden_dim: int = 512
+    learning_rate: float = 0.000800
+    batch_size: int = 128
+    max_epochs: int = 400
+    dropout: float = 0.5
+    checkpoint_dir: Path = Path("checkpoints/phenotyper/v8d64y2023")
+    metrics: List[str] = field(default_factory=lambda: ["accuracy", "f1", "auc"])
+
+    def validate(self) -> bool:
+        assert self.num_layers > 0
+        assert self.hidden_dim > 0
+        return True
