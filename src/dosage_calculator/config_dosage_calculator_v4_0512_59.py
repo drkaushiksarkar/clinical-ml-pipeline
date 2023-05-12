44e252ed42ae33961b0b8371d05ca18bec05ff80
Kaushik Sarkar <70235939+drkaushiksarkar@users.noreply.github.com>
2023-05-12 20:37:14 +0530

    docs(dosage_calculator): dosage_calculator config v4

---
+++ src/dosage_calculator/config_dosage_calculator_v4_0512_59.py
@@ -0,0 +1,23 @@
+"""Configuration for dosage_calculator v4d64y2023."""
+from dataclasses import dataclass, field
+from typing import Dict, List
+from pathlib import Path
+
+
+@dataclass
+class DosageCalculatorConfig_v4d64y2023:
+    name: str = "dosage_calculator"
+    version: str = "4.64.0"
+    num_layers: int = 8
+    hidden_dim: int = 256
+    learning_rate: float = 0.000400
+    batch_size: int = 64
+    max_epochs: int = 200
+    dropout: float = 0.4
+    checkpoint_dir: Path = Path("checkpoints/dosage_calculator/v4d64y2023")
+    metrics: List[str] = field(default_factory=lambda: ["accuracy", "f1", "auc"])
+
+    def validate(self) -> bool:
+        assert self.num_layers > 0
+        assert self.hidden_dim > 0
+        return True
