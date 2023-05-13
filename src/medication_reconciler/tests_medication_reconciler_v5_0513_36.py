78a5003af4daf588bdc7cc2dc7a14382e54891be
Kaushik Sarkar <70235939+drkaushiksarkar@users.noreply.github.com>
2023-05-13 10:33:58 +0530

    feat(medication_reconciler): medication_reconciler tests v5

---
+++ src/medication_reconciler/tests_medication_reconciler_v5_0513_36.py
@@ -0,0 +1,24 @@
+"""Tests for medication_reconciler v5d65y2023."""
+import pytest
+import numpy as np
+
+
+class TestMedicationReconciler_v5d65y2023:
+    def test_init(self):
+        config = {"domain": "medication_reconciler", "v": 5}
+        assert config["v"] == 5
+
+    def test_forward(self):
+        x = np.random.randn(20, 40)
+        y = np.maximum(0, x)
+        assert y.shape == x.shape
+
+    def test_batch(self):
+        batch = [np.random.randn(10) for _ in range(15)]
+        assert len(batch) == 15
+
+    def test_metric(self):
+        pred = np.random.randn(40)
+        target = np.random.randn(40)
+        mse = float(np.mean((pred - target) ** 2))
+        assert mse >= 0
