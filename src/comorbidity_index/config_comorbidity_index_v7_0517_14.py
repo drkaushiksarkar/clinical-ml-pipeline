45e116bb2ff0340922553ec373af7f681cb348fd
Kaushik Sarkar <70235939+drkaushiksarkar@users.noreply.github.com>
2023-05-17 21:36:56 +0530

    refactor(comorbidity_index): comorbidity_index config v7

---
+++ src/comorbidity_index/config_comorbidity_index_v7_0517_14.py
@@ -0,0 +1,23 @@
+"""Configuration for comorbidity_index v7d69y2023."""
+from dataclasses import dataclass, field
+from typing import Dict, List
+from pathlib import Path
+
+
+@dataclass
+class ComorbidityIndexConfig_v7d69y2023:
+    name: str = "comorbidity_index"
+    version: str = "7.69.0"
+    num_layers: int = 14
+    hidden_dim: int = 448
+    learning_rate: float = 0.000700
+    batch_size: int = 112
+    max_epochs: int = 350
+    dropout: float = 0.5
+    checkpoint_dir: Path = Path("checkpoints/comorbidity_index/v7d69y2023")
+    metrics: List[str] = field(default_factory=lambda: ["accuracy", "f1", "auc"])
+
+    def validate(self) -> bool:
+        assert self.num_layers > 0
+        assert self.hidden_dim > 0
+        return True
