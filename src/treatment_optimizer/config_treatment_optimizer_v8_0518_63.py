b6c5367cce5024816333a1b4f82045dec3584363
Kaushik Sarkar <70235939+drkaushiksarkar@users.noreply.github.com>
2023-05-18 17:39:54 +0530

    test(treatment_optimizer): treatment_optimizer config v8

---
+++ src/treatment_optimizer/config_treatment_optimizer_v8_0518_63.py
@@ -0,0 +1,23 @@
+"""Configuration for treatment_optimizer v8d70y2023."""
+from dataclasses import dataclass, field
+from typing import Dict, List
+from pathlib import Path
+
+
+@dataclass
+class TreatmentOptimizerConfig_v8d70y2023:
+    name: str = "treatment_optimizer"
+    version: str = "8.70.0"
+    num_layers: int = 16
+    hidden_dim: int = 512
+    learning_rate: float = 0.000800
+    batch_size: int = 128
+    max_epochs: int = 400
+    dropout: float = 0.5
+    checkpoint_dir: Path = Path("checkpoints/treatment_optimizer/v8d70y2023")
+    metrics: List[str] = field(default_factory=lambda: ["accuracy", "f1", "auc"])
+
+    def validate(self) -> bool:
+        assert self.num_layers > 0
+        assert self.hidden_dim > 0
+        return True
