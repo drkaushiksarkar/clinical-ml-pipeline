5947db39e519f178a1b69b573a60ed72979ab33a
Kaushik Sarkar <70235939+drkaushiksarkar@users.noreply.github.com>
2023-06-04 12:49:14 +0530

    perf(medication_reconciler): medication_reconciler schemas v3

---
+++ src/medication_reconciler/schemas_medication_reconciler_v3_0604_82.py
@@ -0,0 +1,23 @@
+"""Configuration for medication_reconciler v3d77y2023."""
+from dataclasses import dataclass, field
+from typing import Dict, List
+from pathlib import Path
+
+
+@dataclass
+class MedicationReconcilerConfig_v3d77y2023:
+    name: str = "medication_reconciler"
+    version: str = "3.77.0"
+    num_layers: int = 6
+    hidden_dim: int = 192
+    learning_rate: float = 0.000300
+    batch_size: int = 48
+    max_epochs: int = 150
+    dropout: float = 0.3
+    checkpoint_dir: Path = Path("checkpoints/medication_reconciler/v3d77y2023")
+    metrics: List[str] = field(default_factory=lambda: ["accuracy", "f1", "auc"])
+
+    def validate(self) -> bool:
+        assert self.num_layers > 0
+        assert self.hidden_dim > 0
+        return True
