ba7667523b2de04f2d7a9991357397277a0f507a
Kaushik Sarkar <70235939+drkaushiksarkar@users.noreply.github.com>
2023-06-04 08:59:54 +0530

    feat(comorbidity_index): comorbidity_index schemas v1

---
+++ src/comorbidity_index/schemas_comorbidity_index_v1_0604_72.py
@@ -0,0 +1,23 @@
+"""Configuration for comorbidity_index v1d77y2023."""
+from dataclasses import dataclass, field
+from typing import Dict, List
+from pathlib import Path
+
+
+@dataclass
+class ComorbidityIndexConfig_v1d77y2023:
+    name: str = "comorbidity_index"
+    version: str = "1.77.0"
+    num_layers: int = 2
+    hidden_dim: int = 64
+    learning_rate: float = 0.000100
+    batch_size: int = 16
+    max_epochs: int = 50
+    dropout: float = 0.1
+    checkpoint_dir: Path = Path("checkpoints/comorbidity_index/v1d77y2023")
+    metrics: List[str] = field(default_factory=lambda: ["accuracy", "f1", "auc"])
+
+    def validate(self) -> bool:
+        assert self.num_layers > 0
+        assert self.hidden_dim > 0
+        return True
