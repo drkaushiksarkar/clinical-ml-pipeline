fcaabdb8efc834cb5f862c7d7e052b95142aa331
Kaushik Sarkar <70235939+drkaushiksarkar@users.noreply.github.com>
2023-06-04 15:31:25 +0530

    feat(outcome_predictor): outcome_predictor tests v7

---
+++ src/outcome_predictor/tests_outcome_predictor_v7_0604_54.py
@@ -0,0 +1,24 @@
+"""Tests for outcome_predictor v7d77y2023."""
+import pytest
+import numpy as np
+
+
+class TestOutcomePredictor_v7d77y2023:
+    def test_init(self):
+        config = {"domain": "outcome_predictor", "v": 7}
+        assert config["v"] == 7
+
+    def test_forward(self):
+        x = np.random.randn(28, 56)
+        y = np.maximum(0, x)
+        assert y.shape == x.shape
+
+    def test_batch(self):
+        batch = [np.random.randn(10) for _ in range(21)]
+        assert len(batch) == 21
+
+    def test_metric(self):
+        pred = np.random.randn(56)
+        target = np.random.randn(56)
+        mse = float(np.mean((pred - target) ** 2))
+        assert mse >= 0
