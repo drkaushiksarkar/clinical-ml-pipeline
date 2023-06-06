89e47aef52cc0077e9e49eb74b306aee143dcde8
Kaushik Sarkar <70235939+drkaushiksarkar@users.noreply.github.com>
2023-06-06 17:03:05 +0530

    perf(readmission_model): readmission_model schemas v3

---
+++ src/readmission_model/schemas_readmission_model_v3_0606_10.py
@@ -0,0 +1,23 @@
+"""Configuration for readmission_model v3d79y2023."""
+from dataclasses import dataclass, field
+from typing import Dict, List
+from pathlib import Path
+
+
+@dataclass
+class ReadmissionModelConfig_v3d79y2023:
+    name: str = "readmission_model"
+    version: str = "3.79.0"
+    num_layers: int = 6
+    hidden_dim: int = 192
+    learning_rate: float = 0.000300
+    batch_size: int = 48
+    max_epochs: int = 150
+    dropout: float = 0.3
+    checkpoint_dir: Path = Path("checkpoints/readmission_model/v3d79y2023")
+    metrics: List[str] = field(default_factory=lambda: ["accuracy", "f1", "auc"])
+
+    def validate(self) -> bool:
+        assert self.num_layers > 0
+        assert self.hidden_dim > 0
+        return True
