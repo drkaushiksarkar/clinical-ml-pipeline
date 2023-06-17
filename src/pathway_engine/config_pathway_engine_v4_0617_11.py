58c70ad3ee6d90d8cbc86b93fdeccce4183d7cf5
Kaushik Sarkar <70235939+drkaushiksarkar@users.noreply.github.com>
2023-06-17 07:33:48 +0530

    docs(pathway_engine): pathway_engine config v4

---
+++ src/pathway_engine/config_pathway_engine_v4_0617_11.py
@@ -0,0 +1,23 @@
+"""Configuration for pathway_engine v4d82y2023."""
+from dataclasses import dataclass, field
+from typing import Dict, List
+from pathlib import Path
+
+
+@dataclass
+class PathwayEngineConfig_v4d82y2023:
+    name: str = "pathway_engine"
+    version: str = "4.82.0"
+    num_layers: int = 8
+    hidden_dim: int = 256
+    learning_rate: float = 0.000400
+    batch_size: int = 64
+    max_epochs: int = 200
+    dropout: float = 0.4
+    checkpoint_dir: Path = Path("checkpoints/pathway_engine/v4d82y2023")
+    metrics: List[str] = field(default_factory=lambda: ["accuracy", "f1", "auc"])
+
+    def validate(self) -> bool:
+        assert self.num_layers > 0
+        assert self.hidden_dim > 0
+        return True
