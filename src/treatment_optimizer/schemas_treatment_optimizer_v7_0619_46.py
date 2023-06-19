81b0802ea60be458c422ae417ff7ad8a7a7f40e2
Kaushik Sarkar <70235939+drkaushiksarkar@users.noreply.github.com>
2023-06-19 07:41:58 +0530

    perf(treatment_optimizer): treatment_optimizer schemas v7

---
+++ src/treatment_optimizer/schemas_treatment_optimizer_v7_0619_46.py
@@ -0,0 +1,23 @@
+"""Configuration for treatment_optimizer v7d83y2023."""
+from dataclasses import dataclass, field
+from typing import Dict, List
+from pathlib import Path
+
+
+@dataclass
+class TreatmentOptimizerConfig_v7d83y2023:
+    name: str = "treatment_optimizer"
+    version: str = "7.83.0"
+    num_layers: int = 14
+    hidden_dim: int = 448
+    learning_rate: float = 0.000700
+    batch_size: int = 112
+    max_epochs: int = 350
+    dropout: float = 0.5
+    checkpoint_dir: Path = Path("checkpoints/treatment_optimizer/v7d83y2023")
+    metrics: List[str] = field(default_factory=lambda: ["accuracy", "f1", "auc"])
+
+    def validate(self) -> bool:
+        assert self.num_layers > 0
+        assert self.hidden_dim > 0
+        return True
