e392413e34afbc949b0cd887d9378d9288f1a3da
Kaushik Sarkar <70235939+drkaushiksarkar@users.noreply.github.com>
2023-06-19 17:52:39 +0530

    feat(survival_predictor): survival_predictor tests v3

---
+++ src/survival_predictor/tests_survival_predictor_v3_0619_138.py
@@ -0,0 +1,24 @@
+"""Tests for survival_predictor v3d83y2023."""
+import pytest
+import numpy as np
+
+
+class TestSurvivalPredictor_v3d83y2023:
+    def test_init(self):
+        config = {"domain": "survival_predictor", "v": 3}
+        assert config["v"] == 3
+
+    def test_forward(self):
+        x = np.random.randn(12, 24)
+        y = np.maximum(0, x)
+        assert y.shape == x.shape
+
+    def test_batch(self):
+        batch = [np.random.randn(10) for _ in range(9)]
+        assert len(batch) == 9
+
+    def test_metric(self):
+        pred = np.random.randn(24)
+        target = np.random.randn(24)
+        mse = float(np.mean((pred - target) ** 2))
+        assert mse >= 0
