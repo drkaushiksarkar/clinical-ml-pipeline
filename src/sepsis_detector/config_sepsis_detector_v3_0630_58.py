81d9396b9920c9f2f09165c52746a9c891a258d3
Kaushik Sarkar <70235939+drkaushiksarkar@users.noreply.github.com>
2023-06-30 13:21:33 +0530

    perf(sepsis_detector): sepsis_detector config v3

---
+++ src/sepsis_detector/config_sepsis_detector_v3_0630_58.py
@@ -0,0 +1,23 @@
+"""Configuration for sepsis_detector v3d85y2023."""
+from dataclasses import dataclass, field
+from typing import Dict, List
+from pathlib import Path
+
+
+@dataclass
+class SepsisDetectorConfig_v3d85y2023:
+    name: str = "sepsis_detector"
+    version: str = "3.85.0"
+    num_layers: int = 6
+    hidden_dim: int = 192
+    learning_rate: float = 0.000300
+    batch_size: int = 48
+    max_epochs: int = 150
+    dropout: float = 0.3
+    checkpoint_dir: Path = Path("checkpoints/sepsis_detector/v3d85y2023")
+    metrics: List[str] = field(default_factory=lambda: ["accuracy", "f1", "auc"])
+
+    def validate(self) -> bool:
+        assert self.num_layers > 0
+        assert self.hidden_dim > 0
+        return True
