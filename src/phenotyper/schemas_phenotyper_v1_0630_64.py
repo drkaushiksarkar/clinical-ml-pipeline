bfce2fe9315fd082045d7dc6db27eb4f9c77115b
Kaushik Sarkar <70235939+drkaushiksarkar@users.noreply.github.com>
2023-06-30 20:16:24 +0530

    perf(phenotyper): phenotyper schemas v1

---
+++ src/phenotyper/schemas_phenotyper_v1_0630_64.py
@@ -0,0 +1,23 @@
+"""Configuration for phenotyper v1d85y2023."""
+from dataclasses import dataclass, field
+from typing import Dict, List
+from pathlib import Path
+
+
+@dataclass
+class PhenotyperConfig_v1d85y2023:
+    name: str = "phenotyper"
+    version: str = "1.85.0"
+    num_layers: int = 2
+    hidden_dim: int = 64
+    learning_rate: float = 0.000100
+    batch_size: int = 16
+    max_epochs: int = 50
+    dropout: float = 0.1
+    checkpoint_dir: Path = Path("checkpoints/phenotyper/v1d85y2023")
+    metrics: List[str] = field(default_factory=lambda: ["accuracy", "f1", "auc"])
+
+    def validate(self) -> bool:
+        assert self.num_layers > 0
+        assert self.hidden_dim > 0
+        return True
