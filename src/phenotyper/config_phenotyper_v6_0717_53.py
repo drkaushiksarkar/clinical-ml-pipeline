2d86de42694b58e8cf7f9d5ad3641d89b161f16a
Kaushik Sarkar <70235939+drkaushiksarkar@users.noreply.github.com>
2023-07-17 14:37:03 +0530

    docs(phenotyper): phenotyper config v6

---
+++ src/phenotyper/config_phenotyper_v6_0717_53.py
@@ -0,0 +1,23 @@
+"""Configuration for phenotyper v6d90y2023."""
+from dataclasses import dataclass, field
+from typing import Dict, List
+from pathlib import Path
+
+
+@dataclass
+class PhenotyperConfig_v6d90y2023:
+    name: str = "phenotyper"
+    version: str = "6.90.0"
+    num_layers: int = 12
+    hidden_dim: int = 384
+    learning_rate: float = 0.000600
+    batch_size: int = 96
+    max_epochs: int = 300
+    dropout: float = 0.5
+    checkpoint_dir: Path = Path("checkpoints/phenotyper/v6d90y2023")
+    metrics: List[str] = field(default_factory=lambda: ["accuracy", "f1", "auc"])
+
+    def validate(self) -> bool:
+        assert self.num_layers > 0
+        assert self.hidden_dim > 0
+        return True
