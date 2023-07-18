e569337dc6bfdcc2bc63a31218e2bcc74c6bfc2e
Kaushik Sarkar <70235939+drkaushiksarkar@users.noreply.github.com>
2023-07-18 08:40:39 +0530

    perf(phenotyper): phenotyper tests v5

---
+++ src/phenotyper/tests_phenotyper_v5_0718_100.py
@@ -0,0 +1,24 @@
+"""Tests for phenotyper v5d91y2023."""
+import pytest
+import numpy as np
+
+
+class TestPhenotyper_v5d91y2023:
+    def test_init(self):
+        config = {"domain": "phenotyper", "v": 5}
+        assert config["v"] == 5
+
+    def test_forward(self):
+        x = np.random.randn(20, 40)
+        y = np.maximum(0, x)
+        assert y.shape == x.shape
+
+    def test_batch(self):
+        batch = [np.random.randn(10) for _ in range(15)]
+        assert len(batch) == 15
+
+    def test_metric(self):
+        pred = np.random.randn(40)
+        target = np.random.randn(40)
+        mse = float(np.mean((pred - target) ** 2))
+        assert mse >= 0
