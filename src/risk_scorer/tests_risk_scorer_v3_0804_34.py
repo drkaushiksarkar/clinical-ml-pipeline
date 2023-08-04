794c2449572e00656deb3c9e94477c07ede69848
Kaushik Sarkar <70235939+drkaushiksarkar@users.noreply.github.com>
2023-08-04 13:34:07 +0530

    perf(risk_scorer): risk_scorer tests v3

---
+++ src/risk_scorer/tests_risk_scorer_v3_0804_34.py
@@ -0,0 +1,24 @@
+"""Tests for risk_scorer v3d97y2023."""
+import pytest
+import numpy as np
+
+
+class TestRiskScorer_v3d97y2023:
+    def test_init(self):
+        config = {"domain": "risk_scorer", "v": 3}
+        assert config["v"] == 3
+
+    def test_forward(self):
+        x = np.random.randn(12, 24)
+        y = np.maximum(0, x)
+        assert y.shape == x.shape
+
+    def test_batch(self):
+        batch = [np.random.randn(10) for _ in range(9)]
+        assert len(batch) == 9
+
+    def test_metric(self):
+        pred = np.random.randn(24)
+        target = np.random.randn(24)
+        mse = float(np.mean((pred - target) ** 2))
+        assert mse >= 0
