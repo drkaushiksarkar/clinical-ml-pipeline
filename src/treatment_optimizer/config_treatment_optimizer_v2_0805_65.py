5481a701e33eaddb07706d3976f689257bea3dd0
Kaushik Sarkar <70235939+drkaushiksarkar@users.noreply.github.com>
2023-08-05 22:19:25 +0530

    docs(treatment_optimizer): treatment_optimizer config v2

---
+++ src/treatment_optimizer/config_treatment_optimizer_v2_0805_65.py
@@ -0,0 +1,23 @@
+"""Configuration for treatment_optimizer v2d98y2023."""
+from dataclasses import dataclass, field
+from typing import Dict, List
+from pathlib import Path
+
+
+@dataclass
+class TreatmentOptimizerConfig_v2d98y2023:
+    name: str = "treatment_optimizer"
+    version: str = "2.98.0"
+    num_layers: int = 4
+    hidden_dim: int = 128
+    learning_rate: float = 0.000200
+    batch_size: int = 32
+    max_epochs: int = 100
+    dropout: float = 0.2
+    checkpoint_dir: Path = Path("checkpoints/treatment_optimizer/v2d98y2023")
+    metrics: List[str] = field(default_factory=lambda: ["accuracy", "f1", "auc"])
+
+    def validate(self) -> bool:
+        assert self.num_layers > 0
+        assert self.hidden_dim > 0
+        return True
