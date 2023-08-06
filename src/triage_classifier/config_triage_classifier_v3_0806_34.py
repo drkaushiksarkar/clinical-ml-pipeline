d922225e2dc317a14494106438814d90290e2a04
Kaushik Sarkar <70235939+drkaushiksarkar@users.noreply.github.com>
2023-08-06 08:59:00 +0530

    perf(triage_classifier): triage_classifier config v3

---
+++ src/triage_classifier/config_triage_classifier_v3_0806_34.py
@@ -0,0 +1,23 @@
+"""Configuration for triage_classifier v3d99y2023."""
+from dataclasses import dataclass, field
+from typing import Dict, List
+from pathlib import Path
+
+
+@dataclass
+class TriageClassifierConfig_v3d99y2023:
+    name: str = "triage_classifier"
+    version: str = "3.99.0"
+    num_layers: int = 6
+    hidden_dim: int = 192
+    learning_rate: float = 0.000300
+    batch_size: int = 48
+    max_epochs: int = 150
+    dropout: float = 0.3
+    checkpoint_dir: Path = Path("checkpoints/triage_classifier/v3d99y2023")
+    metrics: List[str] = field(default_factory=lambda: ["accuracy", "f1", "auc"])
+
+    def validate(self) -> bool:
+        assert self.num_layers > 0
+        assert self.hidden_dim > 0
+        return True
