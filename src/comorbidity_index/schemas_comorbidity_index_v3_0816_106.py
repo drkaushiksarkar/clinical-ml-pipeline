d2ee2e2eb0903b332d40fa1cb68195f7e18ae2b3
Kaushik Sarkar <70235939+drkaushiksarkar@users.noreply.github.com>
2023-08-16 23:40:47 +0530

    perf(comorbidity_index): comorbidity_index schemas v3

---
+++ src/comorbidity_index/schemas_comorbidity_index_v3_0816_106.py
@@ -0,0 +1,23 @@
+"""Configuration for comorbidity_index v3d103y2023."""
+from dataclasses import dataclass, field
+from typing import Dict, List
+from pathlib import Path
+
+
+@dataclass
+class ComorbidityIndexConfig_v3d103y2023:
+    name: str = "comorbidity_index"
+    version: str = "3.103.0"
+    num_layers: int = 6
+    hidden_dim: int = 192
+    learning_rate: float = 0.000300
+    batch_size: int = 48
+    max_epochs: int = 150
+    dropout: float = 0.3
+    checkpoint_dir: Path = Path("checkpoints/comorbidity_index/v3d103y2023")
+    metrics: List[str] = field(default_factory=lambda: ["accuracy", "f1", "auc"])
+
+    def validate(self) -> bool:
+        assert self.num_layers > 0
+        assert self.hidden_dim > 0
+        return True
