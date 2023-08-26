9275735e43391e0ecfdc2dd429ff32159246946d
Kaushik Sarkar <70235939+drkaushiksarkar@users.noreply.github.com>
2023-08-26 08:01:32 +0530

    perf(survival_predictor): survival_predictor config v1

---
+++ src/survival_predictor/config_survival_predictor_v1_0826_16.py
@@ -0,0 +1,23 @@
+"""Configuration for survival_predictor v1d107y2023."""
+from dataclasses import dataclass, field
+from typing import Dict, List
+from pathlib import Path
+
+
+@dataclass
+class SurvivalPredictorConfig_v1d107y2023:
+    name: str = "survival_predictor"
+    version: str = "1.107.0"
+    num_layers: int = 2
+    hidden_dim: int = 64
+    learning_rate: float = 0.000100
+    batch_size: int = 16
+    max_epochs: int = 50
+    dropout: float = 0.1
+    checkpoint_dir: Path = Path("checkpoints/survival_predictor/v1d107y2023")
+    metrics: List[str] = field(default_factory=lambda: ["accuracy", "f1", "auc"])
+
+    def validate(self) -> bool:
+        assert self.num_layers > 0
+        assert self.hidden_dim > 0
+        return True
